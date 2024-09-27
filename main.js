22164d3de5acdb231787cc831b743c70ad39bf83
rayong <skdyds@gmail.com>
2024-09-27 14:52:47 +0900

    입력값 잡는 것

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //변수
+let score = 0;
 
 const wordInput = document.querySelector(".word-input");
-console.log(wordInput);
+const wordDisplay = document.querySelector(".word-display");
+const scoreDisplay = document.querySelector(".score");
+
+wordInput.addEventListener("input", () => {
+  if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+    score++;
+    scoreDisplay.innerText = score;
+    wordInput.value = "";
+  }
+});
